1d6ca4aac43e6ebfe9347a224237dc36efb836c2
canalmar <marisatcanale@gmail.com>
2025-06-07 11:10:54 -0300

    Mejoro diseño de base e index con imagen destacada y libros recomendados

--- client/views.py
+++ client/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def client_list(request):
+    return HttpResponse("Listado de clientes (vista temporal)")
